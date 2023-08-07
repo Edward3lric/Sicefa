91e37b77e872e322ab908c23bdd42040b5f5cfc2
H4lofthemel <ruizgonzalezmelchor3@gmail.com>
2023-08-07 11:29:52 -0600

    Avances dia 6

--- modules/moduloSucursal/controllerSucursal.js
+++ modules/moduloSucursal/controllerSucursal.js
@@ -1,7 +1,11 @@
 
+// Declara arreglo de sucursales
 var sucursales = [];
+
+// Declarar varable de indece seleccionado
 var indiceSucursalSeleccionada;
 
+// Declara variables globales de los inputs
 var nombreSucursal;
 var nombreTitular;
 var rfcTitular;
@@ -15,7 +19,10 @@ var longitud;
 var latitud;
 var estatus;
 
+// Funcion de inicio
 export function añadirCampos(){
+
+    // Guardar los elementos del DOM que se van a utilizar en varables globales
     nombreSucursal = document.getElementById("nombre-sucursal");
     nombreTitular = document.getElementById("nombre-titular");
     rfcTitular = document.getElementById("rfc-titular");
@@ -29,8 +36,10 @@ export function añadirCampos(){
     latitud = document.getElementById("latitud");
     estatus = document.getElementById("estatus");
 
+    // Limpiar los inputs, por si tenian algo antes
     cleanInputs();
 
+    // Cargar datos desde el archivo JSON
     fetch("modules/moduloSucursal/dataSucursal.json")
     .then(response => {
         return response.json();
@@ -39,12 +48,15 @@ export function añadirCampos(){
             if (sucursales.length === 0){
                 sucursales = jsondata;
             }
+            // Cargar tabla
             loadTable();
         }
     );
 }
 
+// Funcion para añadir una sucursal
 export function addSucursal(){
+    // Recuperar datos de los inputs y asignarlos a su valor correspondiente
     let sucursal = {
         "id_sucursal": sucursales.length + 1,
         "telefono": telefono.value,
@@ -61,17 +73,26 @@ export function addSucursal(){
         "domicilio": domicilio.value
     };
 
+    // Añadir sucursal al arreglo
     sucursales.push(sucursal);
+    // Cargar Tabla
     loadTable();
-
+    // Aplicar filtro de busqueda
+    searchSucursal();
+    // Borrar texto del los inputs
     cleanInputs();
 
+    // Estableser el indice seleccionado en nulo,
+    // Por si tenia algun valor
     indiceSucursalSeleccionada = null;
 }
 
+// Funcion para seleccion una sucursal
 export function selectSucursal(index){
+    // Recuperar la sucursal seleccionada mediante su indice
     let sucursal = sucursales[index]
 
+    // Asignar los valores de la sucursal a los inputs
     nombreSucursal.value = sucursal.nombre;
     nombreTitular.value = sucursal.nombre_titular;
     rfcTitular.value = sucursal.rfc_titular;
@@ -85,18 +106,28 @@ export function selectSucursal(index){
     latitud.value = sucursal.latitud;
     estatus.value = sucursal.estatus;
 
+    // Modificar la variable global con el indice
     indiceSucursalSeleccionada = index;
 }
 
+// Funcion para eliminar una sucurssal
 export function deleteSucursal(){
+    // Cambiar el estatus de la sucursal seleccionada a 0
     sucursales[indiceSucursalSeleccionada].estatus = 0;
+
+    // Establecer el valor del indice seleccionado en nulo
     indiceSucursalSeleccionada = null;
 
-    cleanInputs();
+    // Cargar Tabla
     loadTable();
+    // Aplicar filtro de busqueda
+    searchSucursal();
+    // Borrar texto del los inputs
+    cleanInputs();
 }
 
 export function editSucursal(){
+    // Crear un objeto sucursal con los nuevos valores, recuperados de los inputs
     let sucursal = {
         "id_sucursal": sucursales[indiceSucursalSeleccionada].id_sucursal,
         "telefono": telefono.value,
@@ -113,19 +144,67 @@ export function editSucursal(){
         "domicilio": domicilio.value
     };
 
+    // Asignar el objeto sucursal a la posicion donde estaba el objeto anterior
     sucursales[indiceSucursalSeleccionada] = sucursal;
 
+    // Cargar Tabla
     loadTable();
+    // Aplicar filtro de busqueda
+    searchSucursal();
+    // Borrar texto del los inputs
     cleanInputs();
 
+    // Establecer el valor del indice seleccionado en nulo
     indiceSucursalSeleccionada = null;
 }
 
 export function showSucursal(){
-    loadTable()
+    // Cargar Tabla
+    loadTable();
+    // Aplicar filtro de busqueda
+    searchSucursal();
 }
 
+export function searchSucursal(){
+    // Traer texto a buscar en minusculas para que la busqueda se indistinta de mayusculas o minuculas
+    let textToSeach = document.querySelector("input[type='search']").value.toLowerCase();
+    // Encontrar todas las filas que se encuentran en el tbody "tblSucursal"
+    let tbody = document.getElementById("tblSucursal");
+    let rows = tbody.getElementsByTagName("tr");
+
+    // Codigo para evitar que el input search funcione mo "submit" en el formulario
+    let formulario = document.getElementById("filtros");
+    formulario.addEventListener("submit", function(event) {
+        event.preventDefault();
+    });
+
+    // Recorrer cada una de las filas
+    for (let i = 0; i < rows.length; i++) {
+        // Ocultar la fila con una propiedad CSS
+        rows[i].style.display = "none"
+        // Obtener todos los td dentro de esa fila
+        let celdas = rows[i].getElementsByTagName("td");
+
+        // Recorrer cada tb de la fila
+        for (let j = 0; j < celdas.length; j++){
+
+            // Obtener el texto del td en minusculas para que la busqueda se indistinta de mayusculas o minuculas
+            let text = celdas[j].textContent.toLowerCase();
+
+            // Verificar si la palabra buscada esta dentro del texto
+            if (text.indexOf(textToSeach) > -1){
+                // Mostrar la fila
+                rows[i].style.display = "table-row"
+                // Romper el ciclo
+                break;
+            }
+        }   
+    }
+}
+
+// Funcion para limpiar los inputs
 function cleanInputs(){
+    // Poner en todos los inputs cadenas vacias
     nombreSucursal.value = "";
     nombreTitular.value = "";
     rfcTitular.value = "";
@@ -140,19 +219,28 @@ function cleanInputs(){
     estatus.value = "";
 }
 
-
+// Funcion para cargar la tabla
 function loadTable(){
+    // variable donde almacenare el codigo html
     let html = "";
+    // Recorrer todas las sucursales
     sucursales.forEach(function(sucursal){
-        if (sucursal.estatus == 1 || document.getElementById("mostrar-inactivos").checked){
+
+        // Añadir la sucursal al codigo unicamente si se cumplen pr lo menos una de estas dos condiciones
+        // 1- Que el checkbox tenga un valor "True"
+        // 2- Que el estatus de la sucursal sea 1 o activo
+        if (document.getElementById("mostrar-inactivos").checked || sucursal.estatus == 1){
+            // Crear la variable registro
             let registro = 
             "<tr class='row-data' onclick='controller.selectSucursal(" + sucursales.indexOf(sucursal) + ")'>" + 
             "<td>" + sucursal.id_sucursal +
             "</td><td>" + sucursal.nombre +
             "</td><td>" + sucursal.nombre_titular +
             "</td></tr>";
+            // Sumar la variable registro al codigo html
             html += registro;
         }
     });
+    // Insertar el codigo html dentro de la tabla Sucursal
     document.getElementById("tblSucursal").innerHTML = html;
 }
